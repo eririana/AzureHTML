20977804fc5bdfe57cb0351db10912b1b6517055
eririana <rianaeri@gmail.com>
2019-06-26 12:59:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $serverName = "tcp:dicodingserverapp.database.windows.net,1433";
+    $serverName = "tcp:eririanadicoding.database.windows.net,1433";
     $user = "eririana";
     $pass = "L@gin210584";
     $db = "dicodingdb";
@@ -45,7 +45,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration(name, email, job, date) 
+            $sql_insert = "INSERT INTO registration(name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -59,7 +59,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "SELECT * FROM registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
